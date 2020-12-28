6e0df3d86d51ed242355421b4976157b33dea568
Eren Karaboğa <darkbull1073@gmail.com>
2020-12-28 14:28:30 +0300

    Add project files.

---
+++ ConsoleApp58/Musteri.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp58
+{
+    class Musteri
+    {
+       /*public Musteri()
+        {
+            ID = "13116";
+        }*/
+        public static string ID;
+        public  string ad;
+        public string soyad;
+        private string TC;
+
+       
+        
+        public string _tc
+        {
+            get
+            {
+
+                return this.TC;
+            }
+
+            set
+            {
+                this.TC = value;
+
+            }
+         
+        }
+
+        public static void Hesapla()
+        {
+            Console.WriteLine(Carp(1, 2)); 
+        }
+
+        public static int Hesapla2(int d,int f)
+        {
+
+            return d + f;
+
+        }
+   
+        public static int Carp(int x,int y)
+        {
+
+            return  Hesapla2(5,6)* 2;
+            
+        }
+    
+       
+    }
+}
---
+++ ConsoleApp58/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri.ID = "123546546";
+            Musteri Eren = new Musteri();
+            Eren.ad = "Eren";
+            Eren.soyad = "Karaboğa";
+            Eren._tc = "1656813116";
+            Musteri Ceren = new Musteri();
+            Musteri.Hesapla();
+            
+
+
+            Console.WriteLine(Musteri.Carp(6, 7));
+           
+           
+            Console.ReadLine();
+            
+        
+        }
+
+    }
+
+}   
